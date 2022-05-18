a1e0af620e4516269c9d7e379ca6b3000672a693
pragya9111609602 <pragyabisen14@gmail.com>
2022-05-18 13:13:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import "../src/Style.css"
 
 function App() {
+  const [first,setfirst] = useState("")
+  const [task,settask] = useState([])
+
+  const  handleinput = (e)=> {
+    setfirst(e.target.value)
+  }
+
+  const handleclick = ()=>{
+    const arr = [...task]
+    arr.push(first)
+    settask(arr)
+    setfirst("")
+  }
+
+  const deletetask = (e) => {
+    const arr2 =[...task]
+    arr2.splice(e.target.id, 1)
+    settask(arr2)
+  }
+  var hold = task.map((val, i ) => {
+    return <div className="task"><ol>{val}</ol> <button onClick={deletetask} id={i} >Delete</button></div>
+  } )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+        <div className="box">
+            <h1>To-Do List</h1>
+            <div className="inputField">
+                <input type="text" 
+                onChange={handleinput}
+                placeholder="Add new task..."
+                value={first}
+                />
+                <button className ="addbtn" onClick={handleclick}>Add</button>
+            </div>
+            <div className="container">
+                    {hold}
+            </div>
+        </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
